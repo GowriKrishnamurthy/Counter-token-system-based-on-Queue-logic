a44c3a17262182197aabdab3928d40be76aae923
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-05-03 16:28:17 +1000

    ServeNextCust functionality added

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -23,12 +23,39 @@ namespace CounterQueueImp
         //Next button on counter 1, 2 or 3 clicked will call same method - Next customer to serve
         public void btnCounter1_Click(object sender, EventArgs args)
         {
+            ServeNextCust(txtCounter1, 1);
         }
         public void btnCounter2_Click(object sender, EventArgs args)
         {
+            ServeNextCust(txtCounter2, 2);
         }
         public void btnCounter3_Click(object sender, EventArgs args)
         {
+            ServeNextCust(txtCounter3, 3);
+        }
+
+        // Method to find out who the next customer is from the queue and to which counter
+        public void ServeNextCust(TextBox t, int CounterNum)
+        {
+            // First move the conents of session variable to Queue variable 
+            // and check if there is any customer left in the queue
+            Queue<int> qToken = (Queue<int>)Session["TokenQueue"];
+            if (qToken.Count > 0)
+            {
+                //If there is any customer waiting in the queue, just pick that customer,
+                int whoNext = qToken.Dequeue();
+                txtCounterStatus.Text = "Token Number = " + whoNext + " Please go to Counter" + CounterNum;
+                t.Text = whoNext.ToString();
+
+                //Populate the listbox with the updated Queue variable and update the session variable as well
+                AddTokensToListBox(qToken);
+                Session["TokenQueue"] = qToken;
+            }
+            else
+            {
+                txtCounterStatus.Text = "No customers in the queue";
+                t.Text = "Counter free";
+            }
         }
 
         public void btnPrintToken_Click(object sender, EventArgs args)
@@ -46,9 +73,11 @@ namespace CounterQueueImp
             }
             int nxtToken = (int)Session["LastTokenIssued"] + 1;
             Session["LastTokenIssued"] = nxtToken;
+            //Add new token number to queue
             tokenQueue.Enqueue(nxtToken);
             AddTokensToListBox(tokenQueue);
         }
+        // Add token numbers to the list box for display
         private void AddTokensToListBox(Queue<int> tokenQueue)
         {
             listTokens.Items.Clear();
