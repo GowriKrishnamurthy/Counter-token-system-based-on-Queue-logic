4d02c8b8e1c6d077a2a5bc363f348c192a446f4c
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-05-03 16:14:32 +1000

    Initial ASP contents added

--- Default.aspx
+++ Default.aspx
@@ -1,2 +1,60 @@
 ﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CounterQueueImp._Default" %>
 
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+        <table style="border: 1px solid black; font-family: Arial; text-align: center">
+            <tr>
+                <td><b>Counter 1 </b></td>
+                <td></td>
+                <td><b>Counter 2 </b></td>
+                <td></td>
+                <td><b>Counter 3 </b></td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:TextBox ID="txtCounter1" runat="server">
+                    </asp:TextBox>
+                </td>
+                <td></td>
+                <td>
+                    <asp:TextBox ID="txtCounter2" runat="server">
+                    </asp:TextBox>
+                </td>
+                <td></td>
+                <td>
+                    <asp:TextBox ID="txtCounter3" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button ID="btnCounter1" runat="server" Text="Next" />
+                </td>
+                <td></td>
+                <td>
+                    <asp:Button ID="btnCounter2" runat="server" Text="Next" />
+                </td>
+                <td></td>
+                <td>
+                    <asp:Button ID="btnCounter3" runat="server" Text="Next"  /></td>
+            </tr>
+            <tr>
+                <td colspan="5">
+                    <asp:TextBox ID="txtCounterStatus" runat="server" > </asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="5">
+                    <asp:ListBox ID="listTokens" runat="server"></asp:ListBox>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="5">
+                    <asp:Button ID="btnPrintToken" Text="Print Tokens" runat="server"  /></td>
+            </tr>
+            <tr>
+                <td colspan="5">
+                    <asp:Label ID="lblCurrentStatus" runat="server"
+                        Text="Status">     </asp:Label>
+                </td>
+            </tr>
+        </table>
+</asp:Content>
--- Default.aspx.designer.cs
+++ Default.aspx.designer.cs
@@ -11,5 +11,95 @@ namespace CounterQueueImp {
     
     
     public partial class _Default {
+        
+        /// <summary>
+        /// txtCounter1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCounter1;
+        
+        /// <summary>
+        /// txtCounter2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCounter2;
+        
+        /// <summary>
+        /// txtCounter3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCounter3;
+        
+        /// <summary>
+        /// btnCounter1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCounter1;
+        
+        /// <summary>
+        /// btnCounter2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCounter2;
+        
+        /// <summary>
+        /// btnCounter3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCounter3;
+        
+        /// <summary>
+        /// txtCounterStatus control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCounterStatus;
+        
+        /// <summary>
+        /// listTokens control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox listTokens;
+        
+        /// <summary>
+        /// btnPrintToken control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnPrintToken;
+        
+        /// <summary>
+        /// lblCurrentStatus control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblCurrentStatus;
     }
 }
